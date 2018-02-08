0dff13ef9f4ac1a7d232a9543a372fa2942cf8fd
EmaHaiduc <emma.pasca@yahoo.com>
2018-02-08 18:27:05 +0200

    Testhomework with xpath, css selector and assertThat

--- Cart.java
+++ Cart.java
@@ -18,6 +18,7 @@ public class Cart {
         WebDriver driver= new ChromeDriver();
         driver.get("https://fasttrackit.org/selenium-test/");
         driver.findElement(By.id("search")).sendKeys("pillow"+ Keys.ENTER);
+        System.out.println("Pressed Enter in search field.");
         driver.findElement(By.xpath("//button[contains(@class, 'btn-cart') and ./ ancestor::* [a [@ title= 'Shay Printed Pillow']]]")).click();
         WebElement sucessMessageContainer =driver.findElement(By.cssSelector(".success-msg"));
         assertThat("Product not added to cart.",  sucessMessageContainer.getText(), containsString("PILLOW"));
@@ -35,5 +36,16 @@ public class Cart {
          driver.findElement(By.xpath("//td[@class='product-cart-actions']/input[@value='1']")).clear();
          driver.findElement(By.xpath("//input[@class='input-text qty']")).sendKeys("2"+ Keys.ENTER);
         }
-
+    @Test
+    public void deleteProductFromCart(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nelu\\Desktop\\drivere\\chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://fasttrackit.org/selenium-test/");
+        driver.findElement(By.id("search")).sendKeys("pillow"+ Keys.ENTER);
+        System.out.println("Pressed Enter in search field.");
+        driver.findElement(By.xpath("//button[contains(@class, 'btn-cart') and ./ ancestor::* [a [@ title= 'Shay Printed Pillow']]]")).click();
+        driver.findElement(By.xpath("//td[@class='a-center product-cart-remove last']//a[@title='Remove Item']")).click();
+        WebElement successMessageContainer= driver.findElement(By.className("cart-empty"));
+        assertThat("You have items in your shopping cart", successMessageContainer.getText(), containsString("EMPTY"));
+    }
     }
